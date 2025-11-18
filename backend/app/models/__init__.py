590e02d1f6108459bf3981e12ec6016121716162
SimkiJahara <umme.simki@northsouth.edu>
2025-11-18 11:05:28 +0600

    deleted

--- backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import User, UserRole
-from .movie import Movie, Genre, movie_genre_association
-from .review import Review
-
-__all__ = [
-    "User", "UserRole",
-    "Movie", "Genre", "movie_genre_association",
-    "Review"
-]
