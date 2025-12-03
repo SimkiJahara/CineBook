7738b3038d81ad78cfbcfdfb2bf88e8115ab448b
nawsheennn <nawsheen.sadia@northsouth.edu>
2025-12-03 23:22:48 +0600

    Update main.py
    Use main:app for running uvicorn instead of working:app

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#added the base files 
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+from routers import screening_router
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+# Include screening routes
+app.include_router(screening_router.router)
